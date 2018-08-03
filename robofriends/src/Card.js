f22d40d024cd6d5811c5a30c881cee9ffbba08fb
Thomas <tom.n.knol@gmail.com>
2018-08-03 12:50:12 -0500

    adding robot app

---
+++ robofriends/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+	const Card = ({name,email,id}) => {
+		
+		return (
+			<div className='tc bg-light-green dib br3 ma2 grow bw2 shadow5'>
+				<img src={`https://robohash.org/${id}?150x150`} alt='robots' />
+				<div>
+					<h2>{name}</h2>
+					<p>{email}</p>
+				</div>
+			</div>
+		);
+	}
+
+export default Card;
